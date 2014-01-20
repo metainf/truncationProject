613143f35860280b2725ef517f3ddd8b713f7bca
metainf <tech2007li@hotmail.com>
2014-01-20 01:00:10 -0500

    Update Polyhedron.java

--- PolyHedra/Polyhedron.java
+++ PolyHedra/Polyhedron.java
@@ -11,46 +11,87 @@ public class Polyhedron
 		edges=inputedges;
 		sides=inputsides;
 }
-	public void pointTrun(Vertex point, double distance){
+	public void pointTrun(Vertex point, double chop){
 		ArrayList<Edge> tempEdges=new ArrayList(); //used to store the edges before truncation
-		ArrayList<Face> tempFaces=new ArrayList(); //used to store the faces before truncation
 		ArrayList<Edge> newEdges=new ArrayList();
 		ArrayList<Vertex> newPoints=new ArrayList();
 		ArrayList<Face> newFaces=new ArrayList();
-		for(Edge e : edges){ //generates the list of edges that will be changed
+		ArrayList<Edge> newFace=new ArrayList();// used to make the new face created from truncating.
+		//generates the list of edges that will be changed
+		for(Edge e : edges){
 			if(e.hasPoint(point)){
 				tempEdges.add(e);
 			}
 		}
-		for(Edge e: tempEdges){ //generates the list of faces that will be changed
+		//generates the list of faces that will be changed
+		for(Edge e: tempEdges){
 			for(Face f: sides){
 				if(f.hasEdge(e)){
-					tempFaces.add(f);
+					newFaces.add(f);
 				}
 			}
 		}
-		for(int i=0;i<tempFaces.size();i+=0){ //supermessy way of getting rid of duplicates
-			if(tempFaces.indexOf(tempFaces.get(i)).equals(tempFaces.lastIndexOf(tempFaces.get(i)))){
+		//supermessy way of getting rid of duplicates faces
+		for(int i=0;i<newFaces.size();i+=0){
+			if(newFaces.indexOf(newFaces.get(i)).equals(newFaces.lastIndexOf(newFaces.get(i)))){
 				i++;
 			}
 			else{
-				tempFaces.remove(tempFaces.get(i));
+				newFaces.remove(newFaces.get(i));
 				i=0;
 			}
 		}
-		for(Face f: tempFaces){ //removes the faces that are going to be changed from the actual list
+		//removes the faces that are going to be changed from the actual list
+		for(Face f: newFaces){
 			if(sides.contains(f)){
 				sides.remove(f);
 			}
 		}
-		for(Edge e: tempEdges){ //removes  the edges that are going to be changed from the actual list
+		//removes  the edges that are going to be changed from the actual list
+		for(Edge e: tempEdges){
 			if(edges.contains(e)){
 				edges.remove(e);
 			}
 		}
+		/*This for loop does the truncation, aka, delete the old point, and creates the new point and edges
+		*It will also add the new points and edges to the faces that get chopped.
+		*At the end, the only left is to make the new face created by the truncation
+		*/
 		for(Edge e: tempEdges){
-			
+			//finds the point that does not change in the edge
+			if(e.getVertices()[0].equals(point)){
+				Vertex notTrunPt=e.getVertices()[1];
+			}
+			else{
+				Vertex notTrunPt=e.getVertices()[0];
+			}
+			//creates the vector to get the new point
+			double[] vector={notTrunPt.getX()-point.getX(),
+					 notTrunPt.getY()-point.getY(),
+					 notTrunPt.getZ()-point.getZ()};
+			//creates the new point
+			Vertex newVertex= new Vertex(point.getX()-(vector[0]/e.distance()*chop),
+			                             point.getX()-(vector[1]/e.distance()*chop),
+			                             point.getX()-(vector[2]/e.distance()*chop))
+			//creates the new edge
+			Edge newEdge= new Edge(notTrunpt,newVertex);
+			//adds the new edge to the faces that had the old edge
+			for(int i=0;i<newFaces.size();i++){
+				Face f=newFaces.get(i);
+				if(f.hasEdge(e)){
+					newFaces.remove(f);
+					f.remove(e);
+					f.add(newEdge);
+					newFaces.add(i,f);
+				}
+			}
+			newEdges.add(e);
+			newPoints.add(newVertex);
 		}
+		/* This will create the face that is created by truncating the point
+		*It does so by going through the list of new points, and creating the new face from that
+		*/
+		
 	}
 	public boolean hasVertexAtXY(double x, double y)
 	{
