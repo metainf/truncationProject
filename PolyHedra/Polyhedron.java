180ec75daab83f4c91231b5807bbc9816a27434a
DeerGod <taylorcmurphy@outlook.com>
2014-01-28 17:00:31 -0500

    rotate fixed maybe?

--- PolyHedra/Polyhedron.java
+++ PolyHedra/Polyhedron.java
@@ -591,10 +591,6 @@ public void rotate (double p1X, double p1Y, double p2X, double p2Y)
         Vertex oldVertex;
         Vertex newVertex;
         double [] oldCoords = new double[3];
-        int m,n;
-        n = 0; 
-        m = 0;
-
         for (int i = 0; i < sides.size(); i++)
         {
             for (int v = 0; v < sides.get(i).returnEdges().size(); v ++)
@@ -609,14 +605,14 @@ public void rotate (double p1X, double p1Y, double p2X, double p2Y)
                         -x * Math.cos(b) * Math.sin(c) + z  * (Math.cos(c)  * Math.sin(a) - Math.cos(a) * Math.sin(b) *  Math.sin(c)) + y * (Math.cos(a)  * Math.cos(c) + Math.sin(a) * Math.sin(b) * Math.sin(c)),
                         z * Math.cos(a) * Math.cos(b) - y * Math.sin(a) * Math.cos(b) - x * Math.sin(b));
                         System.out.println("i = " + i + ", v = " + v + ", k = " + k + ", m = " + m + ", n = " + n);
-                    sides.get(i).returnEdges().get(n).getVertexes().set(k, newVertex);
+                    sides.get(i).returnEdges().get(v).getVertexes().set(k, newVertex);
                     //^^ has IndexOutOfBoundsException problems with n and/or m
                     //(icos: n = 3, dodec: n = 5, oct: n = 3, cube: n = 4, tetr: m = 4)
-                    (edges.get(n)).getVertexes().set(k, newVertex);
-                    points.set(m, newVertex);
-                    m++;
+                    (edges.get(v)).getVertexes().set(k, newVertex);
+                    points.set(k, newVertex);
+                  
                 }
-                n ++;
+               
             }
 
         }
