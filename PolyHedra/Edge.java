36c95b790042a13ad7408aafb8b61700ed2a577a
mvrablik <mvrablik@users.noreply.github.com>
2014-06-05 10:59:46 -0400

    Update Edge.java

--- PolyHedra/Edge.java
+++ PolyHedra/Edge.java
@@ -1,22 +1,11 @@
-package PolyHedra;
+package polyhedra;
 
- 
-
-/**
- * Write a description of class Edge here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 import java.util.ArrayList;
 public class Edge
 {
     private Vertex a;
     private Vertex b;
 
-    /**
-     * Constructor for objects of class Edge
-     */
     public Edge(){
         
     }
@@ -25,25 +14,25 @@ public class Edge
         a = q;
         b = w;
     }
-	public Vertex otherVert(Vertex point){
-		if(point.equals(this.a)){
-			return b;
-		}
-		return a;
-	}
+public Vertex otherVert(Vertex point){
+if(point.equals(this.a)){
+return b;
+}
+return a;
+}
     public ArrayList<Vertex> getVertexes()
     {
-        ArrayList<Vertex> points=new ArrayList();
+        ArrayList<Vertex> points=new ArrayList<Vertex>();
         points.add(a);
         points.add(b);
         return points;
     }
     public Vertex[] getVertices()
     {
-    	Vertex[] vertices = new Vertex[2];
-    	vertices[0] = this.a;
-    	vertices[1] = this.b;
-    	return vertices;
+     Vertex[] vertices = new Vertex[2];
+     vertices[0] = this.a;
+     vertices[1] = this.b;
+     return vertices;
     }
     public boolean equals(Edge edge){
             if(edge.hasPoint(a)&&edge.hasPoint(b)){
@@ -54,35 +43,35 @@ public class Edge
             return false;
         }
     }
-	public boolean hasPoint(Vertex v){
-		if(this.getVertices()[0].equals(v)||this.getVertices()[1].equals(v)){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	public double distance()
-	{
-		return	a.distance(b);
-	}
-	public String toString(){
+public boolean hasPoint(Vertex v){
+if(this.getVertices()[0].equals(v)||this.getVertices()[1].equals(v)){
+return true;
+}
+else{
+return false;
+}
+}
+public double distance()
+{
+return	a.distance(b);
+}
+public String toString(){
             String out=""+(a.getX()+b.getX())/2+" "+(a.getY()+b.getY())/2+" "+(a.getZ()+b.getZ())/2;
             return out;
         }
         public void setVerticies(int index, Vertex v)
         {
-        	if (index == 0)
-        	{
-        		a = v;
-        	}
-        	else if (index == 1)
-        	{
-        		b = v;
-        	}
-        	else
-        	{
-        		System.out.println("index must be 0 or 1");
-        	}
+         if (index == 0)
+         {
+         a = v;
+         }
+         else if (index == 1)
+         {
+         b = v;
+         }
+         else
+         {
+         System.out.println("index must be 0 or 1");
+         }
         }
 }
