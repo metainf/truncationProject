10fe04f912b485061535cec6693f89834522d832
mvrablik <mvrablik@users.noreply.github.com>
2014-06-08 22:03:18 -0400

    formatting and small fixes

--- PolyHedra/Edge.java
+++ PolyHedra/Edge.java
@@ -1,25 +1,32 @@
 package polyhedra;
 
 import java.util.ArrayList;
+
 public class Edge
 {
     private Vertex a;
     private Vertex b;
 
-    public Edge(){
+    public Edge()
+    {
         
     }
+    
     public Edge(Vertex q, Vertex w)
     {
         a = q;
         b = w;
     }
-public Vertex otherVert(Vertex point){
-if(point.equals(this.a)){
-return b;
-}
-return a;
-}
+    
+    public Vertex otherVert(Vertex point)
+    {
+    	if(point.equals(this.a))
+    	{
+    		return b;
+    	}
+    	return a;
+    }
+    
     public ArrayList<Vertex> getVertexList()
     {
         ArrayList<Vertex> points=new ArrayList<Vertex>();
@@ -30,61 +37,75 @@ return a;
     
     public Vertex[] getVertices()
     {
-     Vertex[] vertices = new Vertex[2];
-     vertices[0] = this.a;
-     vertices[1] = this.b;
-     return vertices;
+    	Vertex[] vertices = new Vertex[2];
+    	vertices[0] = this.a;
+    	vertices[1] = this.b;
+    	return vertices;
     }
     
     @Override
-    public boolean equals(Object obj){
-        if(obj==null){
-            return false;
-        }
-        if(obj instanceof Edge){
-            return equals((Edge)obj);
-        }
-        return false;
+    public boolean equals(Object obj)
+    {
+    	if(obj==null)
+    	{
+    		return false;
+    	}
+    	if(obj instanceof Edge)
+    	{
+    		return equals((Edge)obj);
+    	}
+    	return false;
     }
-    
-    private boolean equals(Edge edge){
-            if(edge.hasVertex(a)&&edge.hasVertex(b)){
-            return true;
+
+    private boolean equals(Edge edge)
+    {
+    	if(edge.hasVertex(a)&&edge.hasVertex(b))
+    	{
+    		return true;
         }
-    
-        else{
-            return false;
-        }
-    }
-public boolean hasVertex(Vertex v){
-if(this.getVertices()[0].equals(v)||this.getVertices()[1].equals(v)){
-return true;
-}
-else{
-return false;
-}
-}
-public double distance()
-{
-return	a.distanceTo(b);
-}
-public String toString(){
-            String out=""+(a.getX()+b.getX())/2+" "+(a.getY()+b.getY())/2+" "+(a.getZ()+b.getZ())/2;
-            return out;
-        }
-        public void setVertices(int index, Vertex v)
+        else
         {
-         if (index == 0)
-         {
-         a = v;
-         }
-         else if (index == 1)
-         {
-         b = v;
-         }
-         else
-         {
-         System.out.println("index must be 0 or 1");
-         }
+            return false;
         }
+    }
+    
+    public boolean hasVertex(Vertex v)
+    {
+    	if(this.getVertices()[0].equals(v)||this.getVertices()[1].equals(v))
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
+    
+    public double distance()
+    {
+    	return	a.distanceTo(b);
+    }
+    
+    public String toString()
+    {
+    	String out=""+(a.getX()+b.getX())/2+" "+(a.getY()+b.getY())/2
+    			+" "+(a.getZ()+b.getZ())/2;
+    	return out;
+    }
+    
+    public void setVertices(int index, Vertex v)
+    {
+    	if (index == 0)
+    	{
+    		a = v;
+    	}
+    	else if (index == 1)
+    	{
+    		b = v;
+    	}
+    	else
+    	{
+    		System.out.println("index must be 0 or 1");
+    	}
+    }
 }
