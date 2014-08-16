4fea240c46428f6df2a4bc459c55cbb4c831a671
mvrablik <mvrablik@users.noreply.github.com>
2014-08-16 12:55:40 -0400

    Final Update
    Interrupted infinite loop, finished GUI, made misc. bug fixes, cleaned up code

--- PolyHedra/Utility.java
+++ PolyHedra/Utility.java
@@ -4,12 +4,38 @@ import java.util.ArrayList;
 
 /**
  * The utility class for UI
- *
- * @author (Michael Vrablik)
  */
 public class Utility
 {
-    public static String HELP_TEXT = "Help text to be written";
+    public static String INSTRUCTIONS =
+    		"When using the \"Select and truncate\" tool, click on the "
+    		+ "vertex or edge that you wish to truncate.  When it is selected, "
+    		+ "it will become blue, and the truncation slider and button will "
+    		+ "became usable.  Use the slider to select the percentage to "
+    		+ "truncate by, and click the \"Truncate\" button to perform the "
+    		+ "truncation.\n\nWhen using the \"Rotate\" tool, click and drag "
+    		+ "the mouse to rotate the polyhedron.\n\nTo change polyhedra, "
+    		+ "select a new polyhedron from the drop-down list.  If the "
+    		+ "current polyhedron is selected from the list, it will be reset "
+    		+ "to its original state.\n\nHOTKEYS\nSpacebar: Switch tools\nR: "
+    		+ "Reset the current polyhedron\nScroll Wheel (Mouse): Zoom in or "
+    		+ "out\nArrow Keys: Rotate the polyhedron";
+    public static String CREDITS =
+    		"This program was created for Mr. Atlas's 2013-2014 Linear "
+    		+ "Algebra and Kaleidocycles seminar.\n\nHardcore Math: Isaac "
+    		+ "Xia\nDisplay and User Interface: Michael Vrablik\nData "
+    		+ "Structure: Taylor Murphy and Bobby Li\nRotation: Isaac Xia, "
+    		+ "Taylor Murphy, and Michael Vrablik\nTruncation: Bobby Li and "
+    		+ "Michael Vrablik\n\nThe source code for this project can be "
+    		+ "found at https://github.com/mvrablik/truncationProject.  You "
+    		+ "may use code from this project freely as long as full credit "
+    		+ "is given to the original authors.";
+    public static String HELP_TEXT =
+    		"This project is no longer being worked on by the original "
+    		+ "developers, but some major issues remain.  If you are "
+    		+ "interested in further developing it, you are certainly welcome "
+    		+ "to do so.  The source code for this project can be found at "
+    		+ "https://github.com/mvrablik/truncationProject.";
     
     /**
      * Returns (in radians, over the interval [0, 2pi)) the standard angle of the line from (0, 0) to (x, y)
@@ -81,7 +107,6 @@ public class Utility
             smallestDistance=Double.MAX_VALUE;
         }
         edges.add(new Edge(startingVertex,currentVertex));
-        //System.out.println("finished");
         return new Face(edges);
     }
 }
